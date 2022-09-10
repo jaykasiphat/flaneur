47efc368b8543ac527832bd54340b792d3ecfb9a
jaykasiphat <jaykasiphat@gmail.com>
2022-09-11 00:24:27 +0700

    Add travel data

---
+++ src/data.js
@@ -0,0 +1,32 @@
+export default [
+  {
+    title: "Mount Fuji",
+    location: "Japan",
+    googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
+    startDate: "12 Jan, 2021",
+    endDate: "24 Jan, 2021",
+    description:
+      "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
+    imageUrl: "https://source.unsplash.com/WLxQvbMyfas",
+  },
+  {
+    title: "Lake Thun",
+    location: "Switzerland",
+    googleMapsUrl: "https://goo.gl/maps/FfbKQ6edkoYCriRh9",
+    startDate: "16 Sep, 2022",
+    endDate: "20 Sep, 2022",
+    description:
+      "Lake Thun is an Alpine lake in the Bernese Oberland in Switzerland named after the city of Thun, on its northern shore. At 18.6 sq mi in surface area, it is the largest Swiss lake entirely within a single canton.",
+    imageUrl: "https://source.unsplash.com/1XLyzi17Z2M",
+  },
+  {
+    title: "Neuschwanstein Castle",
+    location: "Germany",
+    googleMapsUrl: "https://goo.gl/maps/PcpQvhQ2Zv6f7hcDA",
+    startDate: "11 Nov, 2022",
+    endDate: "21 Nov, 2022",
+    description:
+      "Neuschwanstein Castle is a 19th-century historicist palace on a rugged hill above the village of Hohenschwangau near Füssen in southwest Bavaria, Germany.",
+    imageUrl: "https://source.unsplash.com/IZunjrb3soo",
+  },
+];
