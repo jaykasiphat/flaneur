0ffa4fbb99a00a71b22930b3251200467babc234
jaykasiphat <jaykasiphat@gmail.com>
2022-09-24 15:34:46 +0700

    Add styling

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,16 +1,19 @@
+import pinIcon from '../assets/pin.svg';
+
 export default function Card(props) {
   return (
     <section className="card">
       <img src={props.imageUrl} className="card--img" alt={props.title}/>
       <div className="card--details">
         <p>
-          <span>{props.location}</span>
+          <img src={pinIcon} alt="location icon" className="card--icon--pin" />
+          <span className="card--location">{props.location}</span>
           <span>
-            <a href={props.googleMapsUrl}>View on Google Maps</a>
+            <a href={props.googleMapsUrl} className="card--link">View on Google Maps</a>
           </span>
         </p>
-        <h2>{props.title}</h2>
-        <p>{props.startDate}-{props.endDate}</p>
+        <h2 className="card--title">{props.title}</h2>
+        <p className="card--dates">{props.startDate} - {props.endDate}</p>
         <p>{props.description}</p>
       </div>
     </section>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ export default function Header() {
   return (
     <header>
       <img src={logo} alt="world logo" className="logo--img" />
-      <span className="logo--name">Flaneur</span>
+      <span className="logo--name">flaneur.</span>
     </header>
   );
 }
